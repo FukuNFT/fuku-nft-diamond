492f1e50828de63a013e3fb4f7f53e50ce9620be
Oozyx <hendrik.oosenbrug@gmail.com>
2022-03-21 17:06:10 -0400

    Add options market interface

--- contracts/FukuTypes.sol
+++ contracts/FukuTypes.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+enum OptionDuration {
+    ThirtyDays,
+    NinetyDays
+}
+
 struct BidInputParams {
     bytes12 vault; // the vault from where the funds for the bid originate
     address nft; // the address of the nft collection
@@ -11,4 +16,19 @@ struct BidInputParams {
 struct BidInfo {
     BidInputParams bidInputParams; // the input params used to create bid
     address bidder; // the address of the bidder
+}
+
+struct OptionInputParams {
+    BidInputParams bidInputParams;
+    uint256 premium;
+    OptionDuration duration;
+}
+
+struct OptionInfo {
+    BidInputParams bidInputParams; // the input params used to create base part of bid
+    bool exercisable; // true if option can be exercised, false otherwise
+    address bidder; // the bidder (buyer)
+    address seller; // the nft owner (seller)
+    uint256 premium; // the option premium
+    uint256 optionExpiry; // the option expiry
 }
---
+++ contracts/interfaces/facets/IOptionMarket.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import { OptionDuration, OptionInputParams } from "../../FukuTypes.sol";
+
+interface IOptionMarket {
+    event OptionBidEntered(
+        uint256 optionId,
+        uint256 strike,
+        uint256 premium,
+        uint256 expiry,
+        bytes12 vaultName,
+        address indexed collection,
+        uint256 indexed nftIndex,
+        address indexed bidder
+    );
+
+    event OptionBidWithdrawn(uint256 optionId, address indexed bidder);
+
+    event OptionBidModified(uint256 optionId, uint256 strike, uint256 premium, uint256 expiry);
+
+    event OptionBidAccepted(
+        uint256 optionId,
+        address indexed bidder,
+        address indexed nftOwner,
+        uint256 strike,
+        uint256 optionEnd
+    );
+
+    event OptionExercised(uint256 optionId, address indexed bidder, uint256 strike);
+
+    event OptionExpired(uint256 optionId);
+
+    function placeOptionBid(OptionInputParams calldata optionParams) external;
+
+    function modifyOptionBid(
+        uint256 optionId,
+        uint256 strike,
+        uint256 premium,
+        OptionDuration duration
+    ) external;
+
+    function withdrawOptionBid(uint256 optionId) external;
+
+    function acceptOptionBid(uint256 optionId) external;
+
+    function exerciseOption(uint256 optionId) external;
+
+    function closeOption(uint256 optionId) external;
+}
