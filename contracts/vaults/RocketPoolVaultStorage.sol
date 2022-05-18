49dcc9b46f810b186e46a1784bbab85a189a96aa
ez7212 <ez7212@gmail.com>
2022-05-18 18:13:40 -0400

    import interface and change bytes to address

--- contracts/vaults/RocketPoolVaultStorage.sol
+++ contracts/vaults/RocketPoolVaultStorage.sol
@@ -1,34 +1,33 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-contract RocketPoolVaultStorage {
+import { IRocketPoolVaultStorage } from "../interfaces/vaults/IRocketPoolVaultStorage.sol";
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract RocketPoolVaultStorage is IRocketPoolVaultStorage, Ownable {
     //stores the current vault implementation
     address currentImplementation;
 
-    mapping(bytes32 => address) delegate;
-
-    modifier onlyCurrentImplementation() {
-        require(msg.sender == currentImplementation, "Only the current implementation can call function");
-        _;
-    }
+    mapping(address => address) delegate;
 
     constructor(address _currentImplemntation) {
         currentImplementation = _currentImplemntation;
     }
 
-    function getCurrentImplementation() public view returns (address) {
+    function getCurrentImplementation() public view override returns (address) {
         return currentImplementation;
     }
 
-    function setNewImplementation(address newImplementation) external onlyCurrentImplementation {
+    function setNewImplementation(address newImplementation) external override onlyOwner {
         currentImplementation = newImplementation;
     }
 
-    function setDelegateAddress(bytes32 _data, address delegateAddress) external onlyCurrentImplementation {
-        delegate[_data] = delegateAddress;
+    function setDelegateAddress(address user, address delegateAddress) external override onlyOwner {
+        delegate[user] = delegateAddress;
     }
 
-    function getDelegateAddress(bytes32 _data) external view returns (address) {
-        return delegate[_data];
+    function getDelegateAddress(address user) public view override returns (address) {
+        return delegate[user];
     }
 }
