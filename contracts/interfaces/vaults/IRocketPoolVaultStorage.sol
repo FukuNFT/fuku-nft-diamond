418e5fb8561fdef876919ddc14c02662cb64868e
ez7212 <ez7212@gmail.com>
2022-05-19 10:06:34 -0400

    remove unneeded functions

--- contracts/interfaces/vaults/IRocketPoolVaultStorage.sol
+++ contracts/interfaces/vaults/IRocketPoolVaultStorage.sol
@@ -2,10 +2,6 @@
 pragma solidity ^0.8.0;
 
 interface IRocketPoolVaultStorage {
-    function getCurrentImplementation() external view returns (address);
-
-    function setNewImplementation(address newImplementation) external;
-
     function setDelegateAddress(address user, address delegateAddress) external;
 
     function getDelegateAddress(address user) external view returns (address);
